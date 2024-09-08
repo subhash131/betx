c52e366b456bf503858281318abcfe087e8645d5
subhash131 <subhashnayak131@gmail.com>
2024-09-09 02:32:02 +0530

    add join lobby

---
+++ src/components/join-lobby.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useLobbyPublicKey } from "@/hooks/use-lobby-publickey";
+import { useProgram } from "@/hooks/use-program";
+import { AnchorError, BN } from "@project-serum/anchor";
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { SystemProgram } from "@solana/web3.js";
+import React, { useState } from "react";
+import { toast } from "sonner";
+
+const JoinLobby = () => {
+  const [lobbyId] = useState<number>(2);
+  const wallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const program = useProgram(connection, wallet);
+
+  const joinLobby = async () => {
+    if (!wallet?.publicKey) {
+      toast.error("Please connect your wallet!");
+      return;
+    }
+
+    if (!program) {
+      toast.error("Failed to connect, please refresh the page");
+      return;
+    }
+
+    const lobbyPk = useLobbyPublicKey(Number(lobbyId));
+
+    try {
+      await program.methods
+        .joinLobby(new BN(lobbyId))
+        .accounts({
+          lobby: lobbyPk,
+          authority: wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .rpc();
+    } catch (err) {
+      if (err instanceof AnchorError) {
+        toast.error(err.error.errorMessage);
+      } else {
+        console.log("🚀 ~ createLobby ~ err:", err);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <button className="bg-blue-500 p-2 rounded-md" onClick={joinLobby}>
+        Join lobby
+      </button>
+    </div>
+  );
+};
+
+export default JoinLobby;
