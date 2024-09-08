62bad2216d570b7b4395781664f4ded50efd776e
subhash131 <subhashnayak131@gmail.com>
2024-09-09 02:29:34 +0530

    modularize page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,18 @@
-"use client";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import "@solana/wallet-adapter-react-ui/styles.css";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { useMemo } from "react";
+import WalletButton from "@/components/wallet-button";
+import Username from "@/components/username";
+import RegisterUsername from "@/components/register-user";
+import CreateLobby from "@/components/create-lobby";
+import JoinLobby from "@/components/join-lobby";
+import PlaceBet from "@/components/place-bet";
 
 export default function Home() {
-  const endPoint =
-    "https://mainnet.helius-rpc.com/?api-key=b6f51962-5102-4fdb-a944-bf09ea5c3c14";
-
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
   return (
-    <ConnectionProvider endpoint={endPoint}>
-      <WalletProvider wallets={wallets}>
-        <WalletModalProvider>
-          <div className="flex gap-20">
-            <WalletMultiButton style={{ backgroundColor: "#3b82f6" }} />
-            <button className="bg-blue-500 p-2 rounded-md">
-              Connect Wallet
-            </button>
-            <button className="bg-blue-500 p-2 rounded-md">
-              Connect Wallet
-            </button>
-            <button className="bg-blue-500 p-2 rounded-md">
-              Connect Wallet
-            </button>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div className="flex gap-20">
+      <CreateLobby />
+      <WalletButton />
+      <Username />
+      <JoinLobby />
+      <PlaceBet />
+    </div>
   );
 }
