63757d8e4baf5c4707765efb13625377a9759fc8
subhash131 <subhashnayak131@gmail.com>
2024-09-08 14:02:34 +0530

    project setup

---
+++ src/utils/helpers.ts
@@ -0,0 +1,19 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+
+export const mockWallet = () => {
+  return {};
+};
+
+export const shortenPk = (pk: PublicKey, chars = 5) => {
+  const pkStr = typeof pk === "object" ? pk.toBase58() : pk;
+  return `${pkStr.slice(0, chars)}...${pkStr.slice(-chars)}`;
+};
+
+export const confirmTx = async (txHash: string, connection: Connection) => {
+  const blockhashInfo = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({
+    blockhash: blockhashInfo.blockhash,
+    lastValidBlockHeight: blockhashInfo.lastValidBlockHeight,
+    signature: txHash,
+  });
+};
