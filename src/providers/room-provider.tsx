2770ef5788a9c6e737b37ba43ffcb3da600eb326
subhash131 <subhashnayak131@gmail.com>
2024-09-09 19:27:37 +0530

    create room provider

---
+++ src/providers/room-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider
+      publicApiKey={
+        "pk_dev_Bfpq5Ohfbe_SFvf09EBj799KqQMwWbz1S56k4LazSYCJyI-hJkZHZPocXrLokdh5"
+      }
+    >
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
