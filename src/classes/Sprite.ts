5ae17203c9f25807279769ab7c8641d1b0fedcae
subhash131 <subhashnayak131@gmail.com>
2024-09-09 19:28:04 +0530

    add update method to Sprite class

--- src/classes/Sprite.ts
+++ src/classes/Sprite.ts
@@ -36,8 +36,16 @@ export class Sprite {
 
   update() {
     this.draw();
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
+    if (this.position.y < 0) {
+      this.position.y += 1;
+    } else {
+      this.position.y += this.velocity.y;
+    }
+    if (this.position.x < 0) {
+      this.position.x += 1;
+    } else {
+      this.position.x += this.velocity.x;
+    }
     if (this.position.y + this.height >= this.canvas.height) {
       this.velocity.y = 0;
     } else {
