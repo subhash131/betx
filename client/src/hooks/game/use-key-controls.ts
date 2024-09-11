8708c6e28a17fa70f964a1827de88e81cd441378
subhash131 <subhashnayak131@gmail.com>
2024-09-11 19:27:41 +0530

    temporarily remove key controls

--- client/src/hooks/game/use-key-controls.ts
+++ client/src/hooks/game/use-key-controls.ts
@@ -1,68 +1,16 @@
-"use client";
-import { useRoom } from "@liveblocks/react";
-import { useEffect, useState } from "react";
+// "use client";
+// import { useRoom } from "@/providers/room-provider";
+// import { useEffect } from "react";
 
-const initKeys = {
-  lastKey: "",
-  a: {
-    pressed: false,
-  },
-  d: {
-    pressed: false,
-  },
-  w: {
-    pressed: false,
-  },
-};
-export const useKeyControls = () => {
-  const [keys, setKeys] = useState(initKeys);
-  const { updatePresence, isPresenceReady } = useRoom();
+// export const useKeyControls = () => {
+//   const { setKeys } = useRoom();
 
-  const handleKeyup = (e: KeyboardEvent) => {
-    if (!isPresenceReady()) return;
-    switch (e.key) {
-      case "d":
-      case "ArrowRight":
-        setKeys((prev) => ({ ...prev, d: { pressed: false } }));
-        break;
-      case "a":
-      case "ArrowLeft":
-        setKeys((prev) => ({ ...prev, a: { pressed: false } }));
-        break;
-      case "w":
-      case "ArrowUp":
-        setKeys((prev) => ({ ...prev, w: { pressed: false } }));
-        break;
-    }
-  };
-
-  const handleKeydown = (e: KeyboardEvent) => {
-    if (!isPresenceReady()) return;
-    switch (e.key) {
-      case "d":
-      case "ArrowRight":
-        setKeys((prev) => ({ ...prev, d: { pressed: true }, lastKey: "d" }));
-        break;
-      case "a":
-      case "ArrowLeft":
-        setKeys((prev) => ({ ...prev, a: { pressed: true }, lastKey: "a" }));
-
-        break;
-      case "w":
-      case "ArrowUp":
-        setKeys((prev) => ({ ...prev, w: { pressed: true }, lastKey: "" }));
-        keys.w.pressed = true;
-        break;
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeydown);
-    document.addEventListener("keyup", handleKeyup);
-    return () => {
-      document.removeEventListener("keydown", handleKeydown);
-      document.removeEventListener("keyup", handleKeyup);
-    };
-  }, []);
-
-  return keys;
-};
+//   useEffect(() => {
+//     document.addEventListener("keydown", handleKeydown);
+//     document.addEventListener("keyup", handleKeyup);
+//     return () => {
+//       document.removeEventListener("keydown", handleKeydown);
+//       document.removeEventListener("keyup", handleKeyup);
+//     };
+//   }, []);
+// };
