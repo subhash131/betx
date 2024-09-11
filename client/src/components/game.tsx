43995b38ae3cedeb74bb35b01d4dccc05844f73a
subhash131 <subhashnayak131@gmail.com>
2024-09-11 19:26:49 +0530

    update game controls

--- client/src/components/game.tsx
+++ client/src/components/game.tsx
@@ -1,45 +1,89 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Sprite } from "@/classes/Sprite";
-import { useMyPresence, useOthers, useRoom } from "@liveblocks/react";
-import { useKeyControls } from "@/hooks/game/use-key-controls";
+
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { io } from "socket.io-client";
+import WalletButton from "./wallet-button";
+import { socket } from "@/socket";
+
+// const sprites: Sprite[] = [];
+export type Players = {
+  [playerId: string]: Sprite;
+};
+
+const players: Players = {};
 
 const Game = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const {} = useRoom();
-  const presence = useMyPresence()[0];
-  const keys = useKeyControls();
-  const others = useOthers();
+  const wallet = useAnchorWallet();
+  const [walletAdd, setWalletAdd] = useState<string>();
+  const newSocket = useMemo(() => io("http://localhost:8000"), [wallet]);
+
+  useEffect(() => {
+    const dpr = devicePixelRatio || 1;
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!ctx || !canvas) {
+      return;
+    }
+    const walletAddress = wallet?.publicKey.toString();
+    if (!walletAddress) return;
+    setWalletAdd(walletAddress);
+    newSocket.emit("walletConnect", walletAddress);
+    console.log("🚀 ~ useEffect ~ walletAddress:", walletAddress);
+
+    newSocket.on("updatePlayers", (_players) => {
+      console.log("🚀 ~ newSocket.on ~ _players:", _players);
+      //add backend players to frontend
+      for (const key in _players) {
+        if (!players[key]) {
+          players[key] = new Sprite({
+            canvas,
+            ctx,
+            position: { x: Math.random() * innerWidth, y: 0 },
+            dpr,
+            color: key == walletAddress ? "blue" : "red",
+          });
+        }
+      }
+      //remove players
+      for (const key in players) {
+        if (!_players[key]) {
+          delete players[key];
+        }
+      }
+    });
+
+    newSocket.on("disconnect", (reason) => {
+      if (!walletAdd) return;
+      delete players[walletAdd];
+      console.log("Disconnected from server, reason:", reason);
+    });
+
+    return () => {
+      newSocket.emit("walletDisconnect", walletAdd);
+      socket.disconnect();
+    };
+  }, [wallet?.publicKey]);
 
   useEffect(() => {
     let animateFrame: number;
     const canvas = canvasRef.current;
     const ctx = canvas?.getContext("2d");
+    const dpr = devicePixelRatio || 1;
     if (canvas) {
-      canvas.width = innerWidth;
-      canvas.height = innerHeight;
+      canvas.width = innerWidth * dpr;
+      canvas.height = innerHeight * dpr;
     }
     if (!ctx || !canvas) return;
 
-    const player = new Sprite({ canvas, ctx, position: { x: 0, y: 0 } });
-    const enemy = new Sprite({ canvas, ctx, position: { x: 200, y: 0 } });
-
     function animate() {
-      if (!player || !enemy || !keys || !presence || !others) return;
-      const { keyControl } = presence;
       if (canvas && ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        player.update();
-        enemy.update();
 
-        // update player
-        player.velocity.x = 0;
-        if (keyControl.a.pressed && keyControl.lastKey === "a") {
-          player.velocity.x = -1;
-        } else if (keyControl.d.pressed && keyControl.lastKey === "d") {
-          player.velocity.x = 1;
-        } else if (keyControl.w.pressed) {
-          player.velocity.y = -10;
+        for (let i in players) {
+          players[i].update();
         }
       }
       animateFrame = requestAnimationFrame(animate);
@@ -50,11 +94,14 @@ const Game = () => {
     return () => {
       cancelAnimationFrame(animateFrame);
     };
-  }, [presence]);
+  }, []);
 
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <canvas ref={canvasRef} className="" />
+    <div className="w-screen h-screen overflow-hidden relative">
+      <div className="absolute top-0">
+        <WalletButton />
+      </div>
+      <canvas ref={canvasRef} className="size-full" />
     </div>
   );
 };
