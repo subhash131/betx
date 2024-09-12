0905cfb24376a4a19e754caaf173f3390fb9e005
subhash131 <subhashnayak131@gmail.com>
2024-09-12 22:38:16 +0530

    add constants: samuraiSPrites

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -7,3 +7,18 @@ export const USER_SEED = "user";
 export const PROGRAM_ID = new PublicKey(
   "DfDwFTWM6f99KRBYEWxShUsbXgJ4K1NR9KbSNWvQVu4H"
 );
+
+export const samuraiSPrites = {
+  idle: {
+    imageSrc: "/samuraiMack/Idle.png",
+    framesMax: 8,
+  },
+  run: {
+    imageSrc: "/samuraiMack/Run.png",
+    framesMax: 8,
+  },
+  attack1: {
+    imageSrc: "/samuraiMack/Attack1.png",
+    framesMax: 8,
+  },
+};
