99b612f2dc6f70f14077225b0511de2f609b4656
subhash131 <subhashnayak131@gmail.com>
2024-09-12 22:37:53 +0530

    remove dead code

--- client/src/app/game/[lobby]/page.tsx
+++ client/src/app/game/[lobby]/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 import Game from "@/components/game";
-// import { Room } from "@/providers/room-provider";
-import { socket } from "@/socket";
-import { useEffect } from "react";
 
 export default function Page({
   params: { lobby },
 }: {
   params: { lobby: string };
 }) {
-  useEffect(() => {
-    socket.on("updateEnemy", (players) => {
-      console.log("🚀 ~ updatePlayers:", players);
-    });
-  }, [socket]);
   return <Game />;
 }
