ba9c464359677620f45e3487854bad1a72dd8533
subhash131 <subhashnayak131@gmail.com>
2024-09-14 20:53:40 +0530

    add component: landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,5 @@
-import WalletButton from "@/components/wallet-button";
-import Username from "@/components/username";
-import RegisterUsername from "@/components/register-user";
-import CreateLobby from "@/components/create-lobby";
-import JoinLobby from "@/components/join-lobby";
-import PlaceBet from "@/components/place-bet";
+import LandingPage from "@/components/landing-page";
 
 export default function Home() {
-  return (
-    <div className="flex gap-20">
-      <CreateLobby />
-      <WalletButton />
-      <Username />
-      <JoinLobby />
-      <PlaceBet />
-    </div>
-  );
+  return <LandingPage />;
 }
