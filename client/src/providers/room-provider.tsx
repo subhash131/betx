3a1c7c1f26a9ab482315decf3f160f88cbacd0fa
subhash131 <subhashnayak131@gmail.com>
2024-09-16 12:59:26 +0530

    update room providers

--- client/src/providers/room-provider.tsx
+++ client/src/providers/room-provider.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  updatePlayerOneId,
+  updatePlayerOnePlacedBet,
+  updatePlayerOneUsername,
+  updatePlayerTwoId,
+  updatePlayerTwoPlacedBet,
+  updatePlayerTwoUsername,
+} from "@/state-manager/features/lobby";
 import React, {
   createContext,
   useCallback,
@@ -7,6 +15,7 @@ import React, {
   useEffect,
   useState,
 } from "react";
+import { useDispatch } from "react-redux";
 import { io, Socket } from "socket.io-client";
 
 const ioServer = process.env.NEXT_PUBLIC_SERVER;
@@ -21,6 +30,7 @@ export const useRoom = () => useContext(Room);
 
 const RoomProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket, setSocket] = useState<Socket>();
+  const dispatch = useDispatch();
 
   const newSocket = useCallback(
     () => io(ioServer || "https://betx.onrender.com"),
@@ -30,6 +40,28 @@ const RoomProvider = ({ children }: { children: React.ReactNode }) => {
     const soc = newSocket();
     setSocket(soc);
   }, []);
+  useEffect(() => {
+    socket?.on("updatedLobby", (data) => {
+      if (data.playerOneId) {
+        dispatch(updatePlayerOneId(data.playerOneId));
+      }
+      if (data.playerOneUsername) {
+        dispatch(updatePlayerOneUsername(data.playerOneUsername));
+      }
+      if (data.playerOneBetPlaced) {
+        dispatch(updatePlayerOnePlacedBet(data.playerOneBetPlaced));
+      }
+      if (data.playerTwoId) {
+        dispatch(updatePlayerTwoId(data.playerTwoId));
+      }
+      if (data.playerTwoUsername) {
+        dispatch(updatePlayerTwoUsername(data.playerTwoUsername));
+      }
+      if (data.playerTwoBetPlaced) {
+        dispatch(updatePlayerTwoPlacedBet(data.playerTwoBetPlaced));
+      }
+    });
+  }, [socket]);
   return <Room.Provider value={{ socket }}>{children}</Room.Provider>;
 };
 
