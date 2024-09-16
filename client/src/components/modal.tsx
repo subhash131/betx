f80d1f61fbe6d5a4495486e6e91c313370651750
subhash131 <subhashnayak131@gmail.com>
2024-09-16 12:59:13 +0530

    update modal with join game

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { RootState } from "@/state-manager/store";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import RegisterForm from "./landing-page/register-form";
 import StartGame from "./landing-page/start-game";
 import JoinGame from "./landing-page/join-game";
@@ -10,7 +10,6 @@ const Modal = () => {
   const { isActive, type } = useSelector(
     (root: RootState) => root.ModalReducer
   );
-  const dispatch = useDispatch();
 
   const getContent = () => {
     switch (type) {
